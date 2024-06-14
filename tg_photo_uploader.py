34b207ede45c4c4cb11149745e9db2d92c0001cf
inf <manskiy2k@gmail.com>
2024-06-14 20:14:37 +0300

    download photos from NASA.

--- tg_photo_uploader.py
+++ tg_photo_uploader.py
@@ -1,4 +1,3 @@
-import json
 import datetime
 import requests
 import os
@@ -30,35 +29,22 @@ def nasa_planetary_requests(token):
 
 
 def nasa_epic_requests(token):
-    # url = (f"https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/jpg/{image_name}.jpg")
-    # payload = {
-    #     "api_key": token
-    # }
-    # response = requests.get(url, params=payload)
-    # response.raise_for_status()
-    # print(response.text)
-    # return
-    with open(f"D:\\321.json", "r") as f:
-        data = json.load(f)
-        for image in data:
-            image_name = image["image"]
-            print(image["date"].split(" ")[0])
-            date = datetime.date.fromisoformat(image["date"].split(" ")[0])
-
-            year = image["date"].split("-")[0]
-            month = image["date"].split("-")[1]
-            day = image["date"].split("-")[2].split(" ")[0]
-            url2 = (f"https://api.nasa.gov/EPIC/archive/natural/{date.year}/{date.month}/{date.day}/png/{image_name}.png")
-            print(url2)
+    url = "https://api.nasa.gov/EPIC/api/natural/images"
+    payload = {
+        "api_key": token
+    }
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()
 
 
 def file_extension(url):
     return os.path.splitext(urlparse(url).path)[1]
 
 
-def saving_images(url, filename, image_format="jpeg", path="images\\"):
+def saving_images(url, filename, image_format="jpeg", path="images\\", payload=()):
     os.makedirs(f"{path}", exist_ok=True)
-    response = requests.get(url)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
     with open(f"{path}{filename}.{image_format}", 'wb') as file:
         file.write(response.content)
@@ -67,21 +53,22 @@ def saving_images(url, filename, image_format="jpeg", path="images\\"):
 def main():
     nasa_token = configure_keys("NASE_KEY")
     path = "images_nasa\\"
-    # for image_name, image_url in enumerate(spacex_request()):
-    #     saving_images(image_url, image_name, file_extension(image_url))
-    # for image in nasa_planetary_requests(nasa_token):
-    #     try:
-    #         print(image["hdurl"])
-    #         saving_images(image["hdurl"], os.path.splitext(os.path.split((urlparse(image["hdurl"])).path)[1])[0],
-    #                       file_extension(image["hdurl"]), path)
-    #     except KeyError:
-    #         print(image["url"])
-    #         saving_images(image["url"], os.path.splitext(os.path.split((urlparse(image["url"])).path)[1])[0],
-    #                       file_extension(image["url"]), path)
-
-    nasa_epic_requests(nasa_token)
-
-
+    for image_name, image_url in enumerate(spacex_request()):
+        saving_images(image_url, image_name, file_extension(image_url))
+    for image in nasa_planetary_requests(nasa_token):
+        try:
+            saving_images(image["hdurl"], os.path.splitext(os.path.split((urlparse(image["hdurl"])).path)[1])[0],
+                          file_extension(image["hdurl"]), path)
+        except KeyError:
+            saving_images(image["url"], os.path.splitext(os.path.split((urlparse(image["url"])).path)[1])[0],
+                          file_extension(image["url"]), path)
+    payload = {"api_key": nasa_token}
+    for image in nasa_epic_requests(nasa_token):
+        image_name = image["image"]
+        date = datetime.date.fromisoformat(image["date"].split(" ")[0])
+        month = image["date"].split("-")[1]
+        url = f"https://api.nasa.gov/EPIC/archive/natural/{date.year}/{month}/{date.day}/png/{image_name}.png"
+        saving_images(url, image_name, "png", "images_nasa_EPIC\\", payload)
 
 
 if __name__ == '__main__':
